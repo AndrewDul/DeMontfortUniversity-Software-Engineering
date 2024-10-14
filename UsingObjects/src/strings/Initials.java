02f4b00614eca868f3a060f730650cc8f468d594
AndrewDul <dulandrew.dev@gmail.com>
2024-10-14 19:17:49 +0100

    Initials-user

--- UsingObjects/src/strings/Initials.java
+++ UsingObjects/src/strings/Initials.java
@@ -1,4 +1,5 @@
 package strings;
+import java.util.Scanner;
 
 public class Initials {
 
@@ -12,6 +13,37 @@ public class Initials {
 		char seciondInitial = name.charAt(spaceIndex+1);
 		
 		System.out.println("Initial: " + firstInitial + seciondInitial);
+		
+		// Convert initials to lowercase
+        char firstLowerCaseInitial = Character.toLowerCase(firstInitial);
+        char secondLowerCaseInitial = Character.toLowerCase(seciondInitial);
+		
+		String emailform= "@email.dmu.ac.uk.";
+		System.out.println("E-mail: "+ firstLowerCaseInitial + secondLowerCaseInitial + emailform);
+		
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Provide your fullname: ");
+		
+		String text = sc.nextLine();
+		
+		char firstInitialUser = text.charAt(0);
+		
+		int spaceIndexUser = text.indexOf(" ");
+		
+		char secondInitialUser = text.charAt(spaceIndexUser+1);
+		
+		// Convert initials to lowercase
+        char firstLowerCaseInitialUser = Character.toLowerCase(firstInitialUser);
+        char secondLowerCaseInitialUser = Character.toLowerCase(secondInitialUser);
+		
+		System.out.println("Initial: " + firstInitialUser + secondInitialUser);
+		System.out.println("E-mail: "+ firstLowerCaseInitialUser + secondLowerCaseInitialUser + emailform);
+		
+		System.out.println("------------------------------------------------------------");
+
+		
+		
 
 	}
 
