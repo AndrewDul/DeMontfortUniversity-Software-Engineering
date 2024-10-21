1a20e80c6425f68a31ade642913e7fb7314a6a70
AndrewDul <dulandrew.dev@gmail.com>
2024-10-21 14:42:09 +0100

    InterfaceTypes-and-PortfolioPlayer

---
+++ PortfolioPlayer/src/main/PlayerApp.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.ArrayList;
+
+import lib.Player;
+
+public class PlayerApp {
+
+	public static String execute(int num, ArrayList<Player> players) {
+		
+		return "";
+	}
+	
+}
